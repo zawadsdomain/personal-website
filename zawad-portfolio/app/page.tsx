f181257ae4d4f4f124920bf45d5885a434534b5b
Zawad Chowdhury <zawadchowdhury53@gmail.com>
2025-06-19 17:57:32 -0400

    start of about section

--- zawad-portfolio/app/page.tsx
+++ zawad-portfolio/app/page.tsx
@@ -166,7 +166,14 @@ export default function Portfolio() {
       </section>
 
       {/* About Section */}
-      
+      <section id = "about" className="py-20 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className = "text-center mb-16">
+          <h2 className="text-4xl font-bold text-gray-900 mb-4">About Me</h2>
+          <div className="w-24 h-1 bg-blue-600 mx-auto"></div>
+          </div>
+        </div>
+      </section>
 
 
 
